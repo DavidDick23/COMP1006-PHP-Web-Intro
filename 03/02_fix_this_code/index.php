412418133e0257b2c8543e38350ca1fb9b4b1720
Dave Dick <david.dck@gmail.com>
2026-01-22 10:46:34 -0500

    Fix this code index.php completed

--- 03/02_fix_this_code/index.php
+++ 03/02_fix_this_code/index.php
@@ -3,22 +3,27 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// Connection variables
 $host = "localhost";
 $dbname = "test";
+$port = "3307"; // <- required to properly connect since XAMMP needed to be reconfigured
 $username = "root";
 $password = "";
 
-$dsn = "mysql:host=$hostdbname;dbname=$dbname";
+// Points to the database
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname";
 
 try 
 {
-    
+    // Attempt to connect to the database
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
+    // Display message if connection is made
     echo "Connected to database!";
 }
 catch (PDOException $e) 
 {
+    // Display message if connection failed
     echo "Database error: " . $e;
 }
